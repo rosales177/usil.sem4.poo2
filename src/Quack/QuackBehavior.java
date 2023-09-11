687302739267e79a2bf05910123512b8162e69e3
rosales177 <rosalespoma15@gmail.com>
2023-09-10 22:57:14 -0500

    Creacion de QuackBehavior

---
+++ src/Quack/QuackBehavior.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Quack;
+
+/**
+ *
+ * @author rosales1015
+ */
+public interface QuackBehavior {
+    
+    public abstract String Quack();
+    
+}
---
+++ src/Quack/Squeak.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Quack;
+
+/**
+ *
+ * @author rosales1015
+ */
+public class Squeak implements QuackBehavior {
+    
+    @Override
+    public String Quack(){
+        return "Squeak";
+    }
+    
+}
